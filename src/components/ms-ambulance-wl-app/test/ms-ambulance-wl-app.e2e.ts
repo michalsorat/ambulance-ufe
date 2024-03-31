73a6b0a0927ebf7beec383774cae2ef817909046
michalsorat <michalsorat@gmail.com>
2024-03-31 20:06:06 +0200

    Test

--- src/components/ms-ambulance-wl-app/test/ms-ambulance-wl-app.e2e.ts
+++ src/components/ms-ambulance-wl-app/test/ms-ambulance-wl-app.e2e.ts
@@ -4,7 +4,7 @@ import { MsAmbulanceWlApp } from '../ms-ambulance-wl-app';
 describe('ms-ambulance-wl-app', () => {
 
   it('renders editor', async () => {
-    const page = await newSpecPage({
+    const page = await newSpecPage({ 
       url: `http://localhost/entry/@new`,
       components: [MsAmbulanceWlApp],
       html: `<ms-ambulance-wl-app base-path="/"></ms-ambulance-wl-app>`,
