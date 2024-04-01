a120b2063ab71bda0a66a3036225c55264c40637
michalsorat <michalsorat@gmail.com>
2024-04-01 19:42:52 +0200

    Ambulance waiting list CRUD operations

--- src/components/ms-ambulance-wl-list/ms-ambulance-wl-list.tsx
+++ src/components/ms-ambulance-wl-list/ms-ambulance-wl-list.tsx
@@ -41,8 +41,8 @@ export class MsAmbulanceWlList {
         ? <div class="error">{this.errorMessage}</div>
         :
          <md-list>
-          {this.waitingPatients.map((patient, index) =>
-            <md-list-item onClick={ () => this.entryClicked.emit(index.toString())}>
+            {this.waitingPatients.map((patient) =>
+              <md-list-item onClick={ () => this.entryClicked.emit(patient.id)}>
               <div slot="headline">{patient.name}</div>
               <div slot="supporting-text">{"Predpokladaný vstup: " + this.isoDateToLocale(patient.estimatedStart)}</div>
                 <md-icon slot="start">person</md-icon>
@@ -50,6 +50,10 @@ export class MsAmbulanceWlList {
             )}
           </md-list>
         }
+        <md-filled-icon-button class="add-button"
+          onclick={() => this.entryClicked.emit("@new")}>
+          <md-icon>add</md-icon>
+         </md-filled-icon-button>
       </Host>
     );
   }
