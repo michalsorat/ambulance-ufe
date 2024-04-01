e372f8f11bf3c77a57fa944f376bc48a553db5dd
michalsorat <michalsorat@gmail.com>
2024-04-01 12:42:57 +0200

    testing files changed

--- src/components/ms-ambulance-wl-app/test/ms-ambulance-wl-app.spec.tsx
+++ src/components/ms-ambulance-wl-app/test/ms-ambulance-wl-app.spec.tsx
@@ -2,17 +2,27 @@ import { newSpecPage } from '@stencil/core/testing';
 import { MsAmbulanceWlApp } from '../ms-ambulance-wl-app';
 
 describe('ms-ambulance-wl-app', () => {
-  it('renders', async () => {
+
+  it('renders editor', async () => {
     const page = await newSpecPage({
+      url: `http://localhost/entry/@new`,
       components: [MsAmbulanceWlApp],
-      html: `<ms-ambulance-wl-app></ms-ambulance-wl-app>`,
+      html: `<ms-ambulance-wl-app base-path="/"></ms-ambulance-wl-app>`,
     });
-    expect(page.root).toEqualHtml(`
-      <ms-ambulance-wl-app>
-        <mock:shadow-root>
-          <slot></slot>
-        </mock:shadow-root>
-      </ms-ambulance-wl-app>
-    `);
+    page.win.navigation = new EventTarget()
+    const child = await page.root.shadowRoot.firstElementChild;
+    expect(child.tagName.toLocaleLowerCase()).toEqual ("ms-ambulance-wl-editor");
+
+  });
+
+  it('renders list', async () => {
+    const page = await newSpecPage({
+      url: `http://localhost/ambulance-wl/`,
+      components: [MsAmbulanceWlApp],
+      html: `<ms-ambulance-wl-app base-path="/ambulance-wl/"></ms-ambulance-wl-app>`,
+    });
+    page.win.navigation = new EventTarget()
+    const child = await page.root.shadowRoot.firstElementChild;
+    expect(child.tagName.toLocaleLowerCase()).toEqual("ms-ambulance-wl-list");
   });
 });
--- src/components/ms-ambulance-wl-editor/test/ms-ambulance-wl-editor.spec.tsx
+++ src/components/ms-ambulance-wl-editor/test/ms-ambulance-wl-editor.spec.tsx
@@ -2,17 +2,17 @@ import { newSpecPage } from '@stencil/core/testing';
 import { MsAmbulanceWlEditor } from '../ms-ambulance-wl-editor';
 
 describe('ms-ambulance-wl-editor', () => {
-  it('renders', async () => {
+  it('buttons shall be of different type', async () => {
     const page = await newSpecPage({
       components: [MsAmbulanceWlEditor],
-      html: `<ms-ambulance-wl-editor></ms-ambulance-wl-editor>`,
+      html: `<ms-ambulance-wl-editor entry-id="@new"></ms-ambulance-wl-editor>`,
     });
-    expect(page.root).toEqualHtml(`
-      <ms-ambulance-wl-editor>
-        <mock:shadow-root>
-          <slot></slot>
-        </mock:shadow-root>
-      </ms-ambulance-wl-editor>
-    `);
+    let items: any = await page.root.shadowRoot.querySelectorAll("md-filled-button");
+    expect(items.length).toEqual(1);
+    items = await page.root.shadowRoot.querySelectorAll("md-outlined-button");
+    expect(items.length).toEqual(1);
+
+    items = await page.root.shadowRoot.querySelectorAll("md-filled-tonal-button");
+    expect(items.length).toEqual(1);
   });
 });
